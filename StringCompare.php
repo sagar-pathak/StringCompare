088afbb4f5c60f47154bd838e082295bef4acc49
Sagar <surfer.sagar@gmail.com>
2014-12-18 11:55:58 +0545

    StringCompare

---
+++ StringCompare.php
@@ -0,0 +1,172 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2014 Sagar Pathak (http://www.sagarpathak.com.np).
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * StringCompare is a simple tool to compare two strings based on percentage given.
+ * For e.g StringCompare('Hello World', 'Hello my world', 90) (master, slave, percentage)
+ * will result to TRUE in this case, because all words of master fields are present
+ * in slave field, 100 percentage available. Comparision will be performed
+ * from master to slave.
+ *
+ * @author Sagar Pathak (http://www.sagarpathak.com.np)
+ * @link http://github.com/sagar1992/StringCompare
+ * @version 1.0
+ */
+/* Default configuration */
+$config['COMPARE_TYPE'] = 'wordcount';
+
+/* Custom error handlers */
+
+class InvalidStringCompareTypeException extends LogicException {
+
+}
+
+interface CompareAlgo {
+
+    public function compareString($master, $slave, $percentage);
+}
+
+class StringCompareFactory {
+
+    public static function compareWith($type) {
+        if ($type == 'wordcount') {
+            return new WordCountMethod();
+        } else if ($type == 'charcount') {
+            return new CharCountMethod();
+        } else {
+            throw new InvalidStringCompareTypeException("Invalid string compare type exception.");
+        }
+    }
+
+}
+
+class WordCountMethod implements CompareAlgo {
+
+    private $percentage;
+    private $statsArray = array();
+
+    private function generateResult($array) {
+        $totalCount = count($array);
+        $count = 0;
+        foreach ($array as $value):
+            if ($value == 0) {
+                continue;
+            } else {
+                $count++;
+            }
+        endforeach;
+        $r_percentage = ($count / $totalCount) * 100;
+        if ($this->percentage <= $r_percentage) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public function compareString($master, $slave, $percentage) {
+        $this->percentage = $percentage;
+        $words = explode(' ', $master);
+        if (!empty($words)) {
+            foreach ($words as $word):
+                if (strlen($word) <= 2)
+                    continue;
+                $this->statsArray[$word] = substr_count($slave, $word);
+            endforeach;
+            return $this->generateResult($this->statsArray);
+        }else {
+            return 0;
+        }
+    }
+
+}
+
+class CharCountMethod implements CompareAlgo {
+
+    private $percentage;
+    private $statsArray = array();
+
+    private function generateResult($array) {
+        $totalCount = count($array);
+        $count = 0;
+        foreach ($array as $value):
+            if ($value < 0) {
+                continue;
+            } else {
+                $count++;
+            }
+        endforeach;
+        $r_percentage = ($count / $totalCount) * 100;
+        if ($this->percentage <= $r_percentage) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public function compareString($master, $slave, $percentage) {
+        $this->percentage = $percentage;
+        $masterCount = count_chars($master, 1);
+        $slaveCount = count_chars($slave, 1);
+        foreach ($masterCount as $key => $count):
+            if (array_key_exists($key, $slaveCount)) {
+                $this->statsArray[$key] = $slaveCount[$key] - $count;
+            } else {
+                $this->statsArray[$key] = '-1';
+            }
+        endforeach;
+        return $this->generateResult($this->statsArray);
+    }
+
+}
+
+class StringCompare {
+
+    private $percentage;
+
+    function __construct($master, $slave, $percentage) {
+        $config = $GLOBALS['config'];
+        $comparision_type = $config['COMPARE_TYP'];
+        $master = $this->formatInput($master);
+        $slave = $this->formatInput($slave);
+
+        $comparator = StringCompareFactory::compareWith($comparision_type);
+        $this->result = $comparator->compareString($master, $slave, $percentage);
+    }
+
+    public function __toString() {
+        return (string) $this->result;
+    }
+
+    protected function formatInput($string) {
+        $string = strtolower($string);
+        return $string;
+    }
+
+}
+
+function StringCompare($master, $slave, $percentage) {
+    return new StringCompare($master, $slave, $percentage);
+}
---
+++ stringcompare.js
@@ -0,0 +1,194 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 Sagar Pathak (http://www.sagarpathak.com.np).
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * StringCompare is a simple tool to compare two strings based on percentage given.
+ * For e.g StringCompare('Hello World', 'Hello my world', 90) (master, slave, percentage)
+ * will result to TRUE in this case, because all words of master fields are present
+ * in slave field, 100 percentage available. Comparision will be performed
+ * from master to slave.
+ *
+ * @author Sagar Pathak (http://www.sagarpathak.com.np)
+ * @link http://github.com/sagar1992/StringCompare
+ * @version 1.0
+ */
+(function (window, undefined) {
+    var options = {
+        'COMPARE_TYPE': 'wordcount'
+    };
+
+    var SC = function (master, slave, percentage) {
+        return new StringCompare(master, slave, percentage).result;
+    };
+
+    var WordCountMethod = function () {
+        var statsArray = Array();
+        this.generateResult = function (array) {
+            var totalCount = array.length;
+            var count = 0;
+            for (var i = 0; i < totalCount; i++) {
+                if (array[i] === 0) {
+                    continue;
+                } else {
+                    count++;
+                }
+            }
+            var r_percentage = (count / totalCount) * 100;
+            return this.percentage <= r_percentage ? 1 : 0;
+        };
+
+        this.compareString = function (master, slave, percentage) {
+            var words = master.split(' ');
+            this.percentage = percentage;
+            if (words.length > 0) {
+                for (var key = 0; key < words.length; key++) {
+                    if (words[key].length <= 2)
+                        continue;
+                    statsArray.push(substr_count(slave, words[key]));
+                }
+                return this.generateResult(statsArray);
+            } else {
+                return 0;
+            }
+        };
+    };
+
+    var CharCountMethod = function () {
+    	var statsArray = Array();
+        this.generateResult = function (array) {
+            var totalCount = 0;
+            var count = 0;
+            for (key in array) {
+            	totalCount++;
+                if (array[key] < 0) {
+                    continue;
+                } else {
+                    count++;
+                }
+            }
+            var r_percentage = (count / totalCount) * 100;
+            return this.percentage <= r_percentage ? 1 : 0;
+        };
+
+        this.compareString = function (master, slave, percentage) {
+        	this.percentage = percentage;
+        	masterCount = count_chars(master);
+        	slaveCount = count_chars(slave);
+        	for(var key in masterCount){
+        		if(slaveCount.hasOwnProperty(key)){
+        			statsArray[key] = slaveCount[key] - masterCount[key];
+        		}else{
+        			statsArray[key] = -1;
+        		}
+        	}
+        	return this.generateResult(statsArray);
+        };
+    };
+
+    function StringCompareFactory() {
+        this.compareWith = function (type) {
+            switch (type) {
+                case 'wordcount':
+                    return new WordCountMethod();
+                    break;
+                case 'charcount':
+                    return new CharCountMethod();
+                    break;
+                default:
+                    errorHandler('Invalid string compare type exception. ');
+                    break;
+            }
+        }
+    }
+
+    var StringCompare = function (master, slave, percentage) {
+        var comparisionType = options.COMPARE_TYP;
+        var comparator = new StringCompareFactory().compareWith(comparisionType);
+        master = formatInput(master);
+        slave = formatInput(slave);
+        this.result = comparator.compareString(master, slave, percentage);
+        return this.result;
+    };
+
+    function errorHandler(message) {
+        console.log(message);
+    }
+
+    function substr_count(string, word) {
+        string = string.replace(/\s{2,}/g, ' ');
+        var words = string.split(' ');
+        var count = 0;
+        for (var i = 0; i < words.length; i++) {
+            if (words[i] === word) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    function count_chars(string){
+    	var wordWithUniqueLetters = string.split("").filter(function(x, n, s) { 
+    			return s.indexOf(x) == n 
+    		}).join("");
+    	var uniqueCount = wordWithUniqueLetters.length;
+    	var duplicateCharCount = Array();
+    	var count;
+    	for(var i = 0; i < uniqueCount; i++ ) {
+    		count = 0;
+    		for(var j = 0; j < string.length; j++ ){
+    			if(wordWithUniqueLetters[i] === string[j])
+    				count++;
+    		}	
+    		duplicateCharCount[wordWithUniqueLetters[i]] = count;
+    	}
+    	return duplicateCharCount;
+    }
+
+    function formatInput(string){
+    	string = string.toLowerCase();
+    	return string;
+    }
+
+    function mergeOptions(obj1, obj2) {
+        var obj3 = {};
+        for (var attrname in obj1) {
+            obj3[attrname] = obj1[attrname];
+        }
+        for (var attrname in obj2) {
+            obj3[attrname] = obj2[attrname];
+        }
+        return obj3;
+    }
+
+    SC.setoptions = function (customOptions) {
+        options = mergeOptions(options, customOptions);
+    };
+
+    /* Assigning SC object to global window object */
+    if (!window.SC) {
+        window.SC = SC;
+    }
+
+})(window);
+
